d61f423ae315ff024cee09fd85063784250ca3a3
friend <israelthankgod@gmail.com>
2022-02-25 09:13:01 +0100

    initial commit

---
+++ Mail client.py
@@ -0,0 +1,33 @@
+import smtplib
+
+print("***MAIL TESTER***")
+print("Welcome to SMTP sender Script... \n")
+choice = input("Press 'L' to see list of server host and port numbers or press 'C' to continue: >> ").upper()
+
+while True:
+    if choice == "L":
+        print("Googlemail: smtp.google.com >> 465")
+        break
+    elif choice == "C":
+        print("**Moving on**")
+        break
+    else:
+        print("Invalid Option, Please choose a valid option.")
+        break
+
+hostname = str(input("Enter Hostname or IP: "))
+port = int(input("Enter Port Number: "))
+username = str(input("Enter your username: "))
+password = str(input("Enter your password: "))
+fromadd = str(input("Enter From Address: "))
+toadd = str(input("Enter TO Address: "))
+msg = str(input("Type Your message: "))
+
+
+server = smtplib.SMTP_SSL(hostname, port)
+server.login(username, password)
+server.sendmail(
+    fromadd,
+    toadd,
+    msg)
+server.quit()
