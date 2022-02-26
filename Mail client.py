d8b69b80131143e99dad1d53a46f1a5bea006d21
friend <israelthankgod@gmail.com>
2022-02-26 18:26:41 +0100

    commit

--- Mail client.py
+++ Mail client.py
@@ -1,21 +1,27 @@
 import smtplib
 
+def msg(message):
+    return message
+
+
 print("***MAIL TESTER***")
 print("**Welcome**")
 print('*welcome*')
 print("Welcome to SMTP sender Script... \n")
-choice = input("Press 'L' to see list of server host and port numbers or press 'C' to continue: >> ").upper()
+
 
 while True:
+    choice = input("Press 'L' to see list of server host and port numbers or press 'C' to continue: >> ").upper()
     if choice == "L":
         print("Googlemail: smtp.google.com >> 465")
         break
     elif choice == "C":
-        print("**Moving on**")
+        print("**Moving on** \n")
         break
     else:
-        print("Invalid Option, Please choose a valid option.")
-        break
+        print("Invalid Option, Please choose a valid option. Please pick an option \n")
+        continue
+
 
 hostname = str(input("[+] Enter Hostname or IP: "))
 port = int(input("[+] Enter Port Number: "))
@@ -23,7 +29,11 @@ username = str(input("[+] Enter your username: "))
 password = str(input("[+] Enter your password: "))
 from_address = str(input("[+] Enter From Address: "))
 to_address = str(input("[+] Enter TO Address: "))
-msg = str(input("[+] Type Your message: "))
+# msg = str(input("[+] Type Your message: "))
+
+message = msg(input("[+] Enter Your Message: "))
+file = open("content.txt", 'w')
+file.write(msg(message))
 
 try:
     server = smtplib.SMTP_SSL(hostname, port)
@@ -31,7 +41,7 @@ try:
     server.sendmail(
         from_address,
         to_address,
-        msg)
+        message)
     server.quit()
     print("Email sent")
 except:
